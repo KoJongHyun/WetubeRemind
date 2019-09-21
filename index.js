4d4918a485c4f0d16d59308a84a4a143b207e341
KoJongHyun <no1kojong@gmail.com>
2019-09-21 23:47:59 +0900

    Using Babel&nodemon

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 
 const PORT = 4000;
 
-function handleListening() {
-  console.log(`Listening on: http://localhost:${PORT}`);
-}
+const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
+const handleHome = (req, res) => res.send('Hello World!!');
+const handleTest = (req, res) => res.send('Test');
 
-app.listen(PORT, handleListening);
+app.get('/', handleHome);
+app.get('/profile', handleTest);
+
+app.listen(PORT, handleListening); 
