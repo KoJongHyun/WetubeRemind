8beabb14c5a3e31e7a4d739f1f9bd38e2576c389
KoJongHyun <no1kojong@gmail.com>
2019-10-04 23:12:36 +0900

    Home Controller

---
+++ db.js
@@ -0,0 +1,38 @@
+export const videos = [
+  {
+    id: 1,
+    title: 'Friday Night',
+    description: 'This is Friday Night',
+    videoFile: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+    views: 10,
+    creator: {
+      id: 1,
+      name: 'Kkojong',
+      email: 'no1kojong@gmail.com'
+    }
+  },
+  {
+    id: 2,
+    title: 'Saturday Night',
+    description: 'This is Saturday Night',
+    videoFile: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+    views: 10,
+    creator: {
+      id: 1,
+      name: 'Kkojong',
+      email: 'no1kojong@gmail.com'
+    }
+  },
+  {
+    id: 3,
+    title: 'Sunday Night',
+    description: 'This is Sunday Night',
+    videoFile: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+    views: 10,
+    creator: {
+      id: 1,
+      name: 'Kkojong',
+      email: 'no1kojong@gmail.com'
+    }
+  }
+]
