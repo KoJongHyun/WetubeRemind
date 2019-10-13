89c91f23cb26b3e6fa8fa4f40a007e46dd20baff
KoJongHyun <no1kojong@gmail.com>
2019-10-13 21:20:57 +0900

    Comment Model

--- models/Video.js
+++ models/Video.js
@@ -17,7 +17,11 @@ const VideoSchema = new mongoose.Schema({
   createdAt: {
     type: Date,
     default: Date.now
-  }
+  },
+  Comments: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Comment'
+  }]
 });
 
 const model = mongoose.model('Video', VideoSchema);
