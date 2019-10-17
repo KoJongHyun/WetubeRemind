e48f4d7576d0ff6a52299a33b322f7fb10bc13b4
KoJongHyun <no1kojong@gmail.com>
2019-10-17 22:25:18 +0900

    Getting Video by ID (Video Detail)

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -24,13 +24,23 @@ export const postUpload = async(req, res) => {
     body: { title, description },
     file: { path }
   } = req;
-    const newVideo = await Video.create({
-      fileUrl: path,
-      title,
-      description
-    });
-    res.redirect(routes.videoDetail(newVideo.id));
+  const newVideo = await Video.create({
+    fileUrl: path,
+    title,
+    description
+  });
+  res.redirect(routes.videoDetail(newVideo.id));
 }
-export const videoDetail = (req, res) => res.render('videoDetail', { pageTitle : 'Video Detail' });
+export const videoDetail = async(req, res) => {
+  const {
+    params: {id}
+  } = req;
+  try {
+    const video = await Video.findById(id);
+    res.render('videoDetail', { pageTitle : 'Video Detail', video });
+  } catch(error) {
+    res.redirect(routes.home);
+  }
+};
 export const editVideo = (req, res) => res.render('editVideo', { pageTitle : '' });
 export const deleteVideo = (req, res) => res.render('deleteVideo', { pageTitle : 'Delete Video' }); 
